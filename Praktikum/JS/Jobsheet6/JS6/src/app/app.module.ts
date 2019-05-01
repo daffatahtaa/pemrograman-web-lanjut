aec51a8084c3dae548bbcf30e32bb7871d6513a6
daffatahtaa <43267539+daffatahtaa@users.noreply.github.com>
2019-05-01 15:12:23 +0700

    js 8 selesai

--- Praktikum/JS/Jobsheet6/JS6/src/app/app.module.ts
+++ Praktikum/JS/Jobsheet6/JS6/src/app/app.module.ts
@@ -1,21 +1,24 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { ReactiveFormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { SignupFormComponent } from './signup-form/signup-form.component';
+import { ContactComponent } from './contact/contact.component';
+import { ReactiveFormComponent } from './reactive-form/reactive-form.component';
+import { TemplateDrivenComponent } from './template-driven/template-driven.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    SignupFormComponent
+    ContactComponent,
+    ReactiveFormComponent,
+    TemplateDrivenComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    FormsModule,
     ReactiveFormsModule
-],
+  ],
   providers: [],
   bootstrap: [AppComponent]
 })
